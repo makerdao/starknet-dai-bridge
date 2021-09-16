06c6b0ed35e35781bf0ecbad5d0ea660baecc18b
Nick Ulven <nickulven@gmail.com>
2021-09-16 00:42:41 -0400

    Add initial L1 escrow and bridge contracts

--- contracts/l1/L1Escrow.sol
+++ contracts/l1/L1Escrow.sol
@@ -1,9 +1,20 @@
 pragma solidity >=0.8.4;
 
-contract L1Escrow {
-  string public test;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-  constructor() {
-    test = 'test';
+contract L1Escrow {
+
+  IERC20 dai;
+
+  constructor(IERC20 _dai) {
+    dai = _dai;
+  }
+
+  function getDAIAddress() external view returns (IERC20) {
+    return dai;
+  }
+
+  function approve(uint256 amount) external {
+    dai.approve(address(this), amount);
   }
 }
