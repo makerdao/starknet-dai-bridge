f142ff610a4c4b3c3bf72c3cbba131862292c7c6
Nick Ulven <nickulven@gmail.com>
2021-11-15 12:07:57 -0500

    Fix format and linting

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,8 @@
 /**
  * Full goerli deploy including any permissions that need to be set.
  */
-import fs from "fs";
 import { getAddressOfNextDeployedContract } from "@makerdao/hardhat-utils";
+import fs from "fs";
 import hre from "hardhat";
 
 import { callFrom, getAddress, save } from "./utils";
@@ -11,7 +11,7 @@ const L1_GOERLI_DAI_ADDRESS = "0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844";
 const L1_GOERLI_STARKNET_ADDRESS = "0x5e6229F2D4d977d20A50219E521dE6Dd694d45cc";
 let NETWORK: string;
 
-async function main() {
+async function main(): Promise<void> {
   const [signer] = await hre.ethers.getSigners();
   if (signer.provider) {
     const network = await signer.provider.getNetwork();
@@ -23,7 +23,7 @@ async function main() {
     fs.mkdirSync(`./deployments/${NETWORK}`, { recursive: true });
   }
   save("DAI", { address: L1_GOERLI_DAI_ADDRESS }, NETWORK);
-  
+
   const account = await deploy(hre, "account", 2, {}, "account-auth");
   const get_this = await deploy(hre, "get_this", 2, {});
   const l2DAI = await deploy(hre, "dai", 2, {
@@ -34,7 +34,9 @@ async function main() {
   await callFrom(registry, "register", [signer.address], account);
   const l1Escrow = await deploy(hre, "L1Escrow", 1, []);
 
-  const futureL1DAIBridgeAddress = await getAddressOfNextDeployedContract(signer);
+  const futureL1DAIBridgeAddress = await getAddressOfNextDeployedContract(
+    signer
+  );
 
   const l2DAIBridge = await deploy(hre, "l2_dai_bridge", 2, {
     caller: BigInt(account.address).toString(),
@@ -44,7 +46,12 @@ async function main() {
     get_this: BigInt(get_this.address).toString(),
   });
   console.log("Initializing dai");
-  await callFrom(l2DAI, "rely", [BigInt(l2DAIBridge.address).toString()], account);
+  await callFrom(
+    l2DAI,
+    "rely",
+    [BigInt(l2DAIBridge.address).toString()],
+    account
+  );
 
   const l1DAIBridge = await deploy(hre, "L1DAIBridge", 1, [
     L1_GOERLI_STARKNET_ADDRESS,
@@ -57,7 +64,9 @@ async function main() {
   const MAX = BigInt(2 ** 256) - BigInt(1);
   await l1Escrow.approve(DAIAddress, l1DAIBridge.address, MAX);
 
-  const futureL1GovernanceRelayAddress = await getAddressOfNextDeployedContract(signer);
+  const futureL1GovernanceRelayAddress = await getAddressOfNextDeployedContract(
+    signer
+  );
 
   const l2GovernanceRelay = await deploy(hre, "l2_governance_relay", 2, {
     l1_governance_relay: BigInt(futureL1GovernanceRelayAddress).toString(),
@@ -65,7 +74,7 @@ async function main() {
     bridge: BigInt(l2DAIBridge.address).toString(),
   });
 
-  const l1GovernanceRelay = await deploy(hre, "L1GovernanceRelay", 1, [
+  await deploy(hre, "L1GovernanceRelay", 1, [
     L1_GOERLI_STARKNET_ADDRESS,
     l2GovernanceRelay.address,
   ]);
@@ -78,25 +87,23 @@ async function deploy(
   calldata: any,
   saveName?: string
 ) {
-  try {
-    const network = layer === 1 ? "ethers" : "starknet";
-    console.log(`Deploying ${contractName}`);
-    const contractFactory = await hre[network].getContractFactory(contractName);
-    let contract;
-    if (layer === 1) {
-      contract = await contractFactory.deploy(...calldata);
-    } else {
-      contract = await contractFactory.deploy(calldata);
-    }
-    const fileName = saveName || contractName;
-    save(fileName, contract, NETWORK);
-    if (layer === 1) {
-      await contract.deployed();
-    }
-    return contract;
-  } catch (err) {
-    console.log(err);
+  const network = layer === 1 ? "ethers" : "starknet";
+  console.log(`Deploying ${contractName}`);
+  const contractFactory = await hre[network].getContractFactory(contractName);
+  let contract;
+  if (layer === 1) {
+    contract = await contractFactory.deploy(...calldata);
+  } else {
+    contract = await contractFactory.deploy(calldata);
   }
+  const fileName = saveName || contractName;
+  save(fileName, contract, NETWORK);
+  if (layer === 1) {
+    await contract.deployed();
+  }
+  return contract;
 }
 
-main();
+main()
+  .then(() => console.log("Successfully deployed"))
+  .catch((err) => console.log(err));
