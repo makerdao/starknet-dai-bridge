d2ff373d3d5446a322fabb21b0ab22fc66881f6b
Nick Ulven <nickulven@gmail.com>
2022-01-19 08:59:42 -0800

    Fix slither

--- contracts/l1/WormholeGUID.sol
+++ contracts/l1/WormholeGUID.sol
@@ -10,11 +10,3 @@ struct WormholeGUID {
   // uint80 nonce;
   // uint48 timestamp;
 }
-
-function bytes32ToAddress(bytes32 addr) pure returns (address) {
-  return address(uint160(uint256(addr)));
-}
-
-function addressToBytes32(address addr) pure returns (bytes32) {
-  return bytes32(uint256(uint160(addr)));
-}
