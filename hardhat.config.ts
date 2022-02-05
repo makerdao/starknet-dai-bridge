99319c8b1ffd199a9b44cce3fd1eb24ad003b9e5
Nick Ulven <nickulven@gmail.com>
2022-02-04 16:57:39 -0800

    Fix commit mistakes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,81 @@
-starknetNetwork: `${process.env.STARKNET_NETWORK}`,
+import "hardhat-gas-reporter";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "solidity-coverage";
+import "@shardlabs/starknet-hardhat-plugin";
+import "./scripts/interact";
+import "./scripts/account";
+import "./scripts/fork";
+
+import { config as dotenvConfig } from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+import { NetworkUserConfig } from "hardhat/types";
+import { resolve } from "path";
+
+dotenvConfig({ path: resolve(__dirname, "./.env") });
+
+const chainIds = {
+  goerli: 5,
+  hardhat: 31337,
+  kovan: 42,
+  mainnet: 1,
+  rinkeby: 4,
+  ropsten: 3,
+};
+
+// Ensure that we have all the environment variables we need.
+const mnemonic: string | undefined = process.env.MNEMONIC;
+if (!mnemonic) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+}
+
+const infuraApiKey: string | undefined = process.env.INFURA_API_KEY;
+if (!infuraApiKey) {
+  throw new Error("Please set your INFURA_API_KEY in a .env file");
+}
+
+function getChainConfig(network: keyof typeof chainIds): NetworkUserConfig {
+  const url: string = `https://${network}.infura.io/v3/${infuraApiKey}`;
+  return {
+    accounts: {
+      count: 10,
+      mnemonic,
+      path: "m/44'/60'/0'/0",
+    },
+    chainId: chainIds[network],
+    url,
+    gasMultiplier: 1.5,
+  };
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: getChainConfig("goerli"),
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    fork: {
+      url: "http://127.0.0.1:8545",
+    },
+    l2: {
+      url: "http://127.0.0.1:5000",
+    },
+    hardhat: {
+      forking: {
+        url: `https://mainnet.infura.io/v3/${infuraApiKey}`,
+        enabled: process.env.NODE_ENV !== "test",
+      },
+      accounts: {
+        count: 10,
+        mnemonic,
+        path: "m/44'/60'/0'/0",
+      },
+    },
+  },
+  mocha: {
+    starknetNetwork: `${process.env.STARKNET_NETWORK}`,
   },
   paths: {
     artifacts: "./artifacts",
