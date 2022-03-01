a838833f1343b3ef5fb5e9ec7bcb6d531bdb5268
Nick Ulven <nickulven@gmail.com>
2022-02-28 20:45:56 -0800

    Fix slither

---
+++ scripts/createSpell.ts
@@ -0,0 +1,40 @@
+import { getRequiredEnv } from "@makerdao/hardhat-utils";
+import fs from "fs";
+import hre from "hardhat";
+
+async function createSpell(): Promise<void> {
+  const NETWORK = hre.network.name;
+
+  const L1_ESCROW_ADDRESS = getRequiredEnv(
+    `${NETWORK.toUpperCase()}_L1_ESCROW_ADDRESS`
+  );
+  const L1_DAI_ADDRESS = getRequiredEnv(
+    `${NETWORK.toUpperCase()}_L1_DAI_ADDRESS`
+  );
+  const L1_DAI_WORMHOLE_BRIDGE_ADDRESS = getRequiredEnv(
+    `${NETWORK.toUpperCase()}_L1_DAI_WORMHOLE_BRIDGE_ADDRESS`
+  );
+
+  const spellContract = `
+    pragma solidity ^0.7.6;
+
+    interface EscrowApprove {
+      function approve(address, address, uint256) external;
+    }
+
+    contract EscrowApproveAction {
+      function approve() external {
+        address escrow = ${L1_ESCROW_ADDRESS};
+        address token = ${L1_DAI_ADDRESS};
+        address approvedAddress = ${L1_DAI_WORMHOLE_BRIDGE_ADDRESS};
+        EscrowApprove(escrow).approve(token, approvedAddress, type(uint256).max);
+      }
+    }
+  `;
+
+  fs.writeFileSync("./contracts/l1/Spells.sol", spellContract);
+}
+
+createSpell()
+  .then(() => console.log("Successfully created"))
+  .catch((err) => console.log(err));
