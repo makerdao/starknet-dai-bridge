321f4becff8c8c89d3397a06239fc867f22aff64
Nick Ulven <nickulven@gmail.com>
2022-02-28 20:56:34 -0800

    Remove unnecessary files

--- contracts/l1/WormholeGUID.sol
+++ contracts/l1/WormholeGUID.sol
@@ -1,18 +1,3 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-// Copyright (C) 2021 Dai Foundation
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 pragma solidity ^0.7.6;
 
 // Standard Maker Wormhole GUID
@@ -24,26 +9,4 @@ struct WormholeGUID {
   uint128 amount;
   uint80 nonce;
   uint48 timestamp;
-}
-
-// solhint-disable-next-line func-visibility
-function bytes32ToAddress(bytes32 addr) pure returns (address) {
-    return address(uint160(uint256(addr)));
-}
-
-// solhint-disable-next-line func-visibility
-function addressToBytes32(address addr) pure returns (bytes32) {
-    return bytes32(uint256(uint160(addr)));
-}
-
-function getGUIDHash(WormholeGUID memory wormholeGUID) pure returns (bytes32 guidHash) {
-    guidHash = keccak256(abi.encode(
-        wormholeGUID.sourceDomain,
-        wormholeGUID.targetDomain,
-        wormholeGUID.receiver,
-        wormholeGUID.operator,
-        wormholeGUID.amount,
-        wormholeGUID.nonce,
-        wormholeGUID.timestamp
-    ));
 }
