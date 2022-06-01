617bb669e9f2944dc414de168e64f2e1d3f96b5c
Nick Ulven <nickulven@gmail.com>
2022-06-01 07:12:18 -0700

    Fix account creation

--- scripts/account.ts
+++ scripts/account.ts
@@ -11,6 +11,13 @@ task("account:create", "Create new L2 account")
       hre.config.starknet.network || DEFAULT_STARKNET_NETWORK;
     const _name = name || "default";
     const account: ArgentAccount = (await hre.starknet.deployAccount("Argent")) as ArgentAccount;
+    await account.multiInvoke([
+      {
+        functionName: "change_guardian",
+        toContract: account.starknetContract,
+        calldata: { new_guardian: BigInt(0) },
+      },
+    ]);
     saveAccount(`account-${_name}`, account, STARKNET_NETWORK);
     console.log(`account-${_name} L2 address:`, account.starknetContract.address);
   });
