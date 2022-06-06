29618dfcf1a719efc9ff4d097e9252e8bf1fe600
Nick Ulven <nickulven@gmail.com>
2022-06-06 09:02:54 -0700

    Fix slither

--- contracts/l1/TeleportGUID.sol
+++ contracts/l1/TeleportGUID.sol
@@ -10,26 +10,3 @@ struct TeleportGUID {
   uint80 nonce;
   uint48 timestamp;
 }
-
-// solhint-disable-next-line func-visibility
-function bytes32ToAddress(bytes32 addr) pure returns (address) {
-    return address(uint160(uint256(addr)));
-}
-
-// solhint-disable-next-line func-visibility
-function addressToBytes32(address addr) pure returns (bytes32) {
-    return bytes32(uint256(uint160(addr)));
-}
-
-// solhint-disable-next-line func-visibility
-function getGUIDHash(TeleportGUID memory teleportGUID) pure returns (bytes32 guidHash) {
-    guidHash = keccak256(abi.encode(
-        teleportGUID.sourceDomain,
-        teleportGUID.targetDomain,
-        teleportGUID.receiver,
-        teleportGUID.operator,
-        teleportGUID.amount,
-        teleportGUID.nonce,
-        teleportGUID.timestamp
-    ));
-}
